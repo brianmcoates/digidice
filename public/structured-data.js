be8f1ecd5e83515e88d835c96d51c9eb0b93802c
Brian Coates <brian.coates@life.church>
2019-08-25 18:31:13 -0500

    Updates metadata information

---
+++ public/structured-data.js
@@ -0,0 +1,15 @@
+<script type="application/ld+json">
+  {
+    "@context" : "http://schema.org",
+    "@type" : "Product",
+    "name" : "Digidice",
+    "brand" : {
+      "@type" : "Brand",
+      "logo" : "https://digidice.app/d20.svg"
+    },
+    "offers" : {
+      "@type" : "Offer",
+      "price" : "free"
+    }
+  }
+  </script>
