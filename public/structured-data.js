bf30258c015578940a0b621b647638babbdaf0a7
Brian Coates <brian.coates@life.church>
2019-09-03 22:58:02 -0500

    fixed structured data.js script

--- public/structured-data.js
+++ public/structured-data.js
@@ -1,15 +1,13 @@
-<script type="application/ld+json">
-  {
-    "@context" : "http://schema.org",
-    "@type" : "Product",
-    "name" : "Digidice",
-    "brand" : {
-      "@type" : "Brand",
-      "logo" : "https://digidice.app/d20.svg"
-    },
-    "offers" : {
-      "@type" : "Offer",
-      "price" : "free"
-    }
+{
+  "@context" : "http://schema.org",
+  "@type" : "Product",
+  "name" : "Digidice",
+  "brand" : {
+    "@type" : "Brand",
+    "logo" : "https://digidice.app/d20.svg"
+  },
+  "offers" : {
+    "@type" : "Offer",
+    "price" : "free"
   }
-  </script>
+}
